040793c091ef703fb98560331833561aadce9be2
Thibault Vinchent <tvinchent@gmail.com>
2022-04-14 12:39:35 +0200

    derniere modif css avant v1 finie

---
+++ v1-vanilla/util.js
@@ -0,0 +1,20 @@
+// recuperation des var passées en URL
+function $_GET(param) {
+    var vars = {};
+    window.location.href.replace( location.hash, '' ).replace( 
+        /[?&]+([^=&]+)=?([^&]*)?/gi, // regexp
+        function( m, key, value ) { // callback
+            vars[key] = value !== undefined ? value : '';
+        }
+    );
+
+    if ( param ) {
+        return vars[param] ? vars[param] : null;	
+    }
+    return vars;
+}
+
+//toggleMenu
+function toggleMenu(){
+    document.getElementById('menuPrincipal').classList.toggle('desactivated');
+}
