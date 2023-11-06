d792bc663b14ed501d1400e3ff63d02f4464b89d
tvinchent <tvinchent@gmail.com>
2023-11-06 13:40:47 +0100

    display products - but not eslint compliant..

--- v3-reactjs/src/components/ProductComponent.jsx
+++ v3-reactjs/src/components/ProductComponent.jsx
@@ -1,9 +1,12 @@
-export function ProductComponent(){
-    return (
-        <>
-            <tr>
-                <td><a href={'/product'}>Test lien</a></td>
-            </tr>
-        </>
-    )
+export function ProductComponent( {product} ) {
+  return (
+    <tr>
+      <td>{product.title}</td>
+      <td className="notOnMobile">
+        <span className={`category ${product.category}`}>{product.category}</span>
+      </td>
+      <td>{product.price}€</td>
+      <td>{Math.round(product.price * 1.20)}€</td>
+    </tr>
+  );
 }
--- v3-reactjs/src/components/ProductsContainerComponent.jsx
+++ v3-reactjs/src/components/ProductsContainerComponent.jsx
@@ -1,40 +1,38 @@
 import { ProductComponent } from './ProductComponent';
+import { useEffect, useState } from 'react';
 
-export function ProductsContainerComponent () {
+export function ProductsContainerComponent() {
+  const [productList, setProductList] = useState([]);
 
-    (function() {
+  useEffect(() => {
+    // Fetch products from the fake store
+    fetch('https://fakestoreapi.com/products')
+      .then((response) => response.json())
+      .then((data) => {
+        setProductList(data);
+      });
+  }, []); // Empty dependency array ensures this effect runs only once
 
-        // recupere les produits du fake store
-        fetch('https://fakestoreapi.com/products')
-        .then(function(response) {
-            return response.json();
-        })
-
-        .then(function(data){
-            console.log(data);
-            return data;
-        })
-
-    })();
-
-    return (
-        <>
-            <section id="products" className="grisclair">
-                <h2>Products managements</h2>
-                <table>
-                    <thead>
-                        <tr className="bleu">
-                            <th>Product name</th>
-                            <th className="notOnMobile">Category</th>
-                            <th>Price</th>
-                            <th>Price (including VAT)</th>
-                        </tr>
-                    </thead>
-                    <tbody id="contentToFeed">
-                        <ProductComponent></ProductComponent>
-                    </tbody>
-                </table>
-            </section>
-        </>
-    )
+  return (
+    <>
+      <section id="products" className="grisclair">
+        <h2>Products management</h2>
+        <table>
+          <thead>
+            <tr className="bleu">
+              <th>Product name</th>
+              <th className="notOnMobile">Category</th>
+              <th>Price</th>
+              <th>Price (including VAT)</th>
+            </tr>
+          </thead>
+          <tbody id="contentToFeed">
+            {productList.map((product) => (
+              <ProductComponent key={product.id} product={product} />
+            ))}
+          </tbody>
+        </table>
+      </section>
+    </>
+  );
 }
